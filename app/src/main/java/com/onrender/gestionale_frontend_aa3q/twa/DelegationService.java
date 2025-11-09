04b45fe4f6a51fd7241651440abef6ac62e79d70
matteomagro4595 <matteo.magro4595@gmail.com>
2025-11-09 18:38:38 +0100

    add assetlinks.json

---
+++ app/src/main/java/com/onrender/gestionale_frontend_aa3q/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.onrender.gestionale_frontend_aa3q.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
